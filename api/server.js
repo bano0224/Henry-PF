5fbbc9bdc362fe7a4e084c47e89cff30d9fb704f
bano0224 <banorodriguez0224@gmail.com>
2021-08-30 23:55:12 -0300

    Henryyy

---
+++ api/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
