e9a7bc8fc18cb41b9804992532aee563044f419f
Joaquin Ignacio de la Torre <81587885+Joaquinidlt@users.noreply.github.com>
2021-09-01 23:07:35 +0200

    Home, NavBar

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import NavBar from '../NavBar/NavBar';
 // import { NavLink } from 'react-router-dom';
 // import {Link } from 'react-router-dom';
-// import style from "./Home.module.css";
+import s from "./Home.module.css";
 
 function Home() {
     
     return (
-        <div className= 'body3'>
+        <div className= {s.body3}>
            <NavBar/>
             <h1> Hello Home InProgress</h1>
            
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 import s from "./NavBar.module.css";
 // import Search from "../Search/Search";
 //---------NAVBAR--------//
 
 export default function NavBar() {
   return (
-    <div className={s.navBar}>
+
+    <nav className={s.topnav}>
+    <Link to='/Categorias'>
+      Categorias
+    </Link>
+    <Link to='/Ofertas'>
+      Ofertas 
+    </Link>
+    <Link to='/Login'>
+      Login 
+    </Link>
+    <Link to='/activity'>
+      Search
+    </Link>
+
+  </nav>
+/*     <div className={s.navBar}>
       <ul className={s.ulNav}>
         <li>
           <NavLink className={s.text} exact to="/CATEGORIAS">
@@ -20,8 +36,8 @@ export default function NavBar() {
             LOGIN
           </NavLink>
         </li>
-        <li>{/* <Search /> */}</li>
+        <li>{/* <Search /> }</li>
       </ul>
-    </div>
+    </div> */
   );
 }
