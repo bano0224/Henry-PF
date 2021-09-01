688b8c63001e1dff82a22de16d641f84ee71450f
Eliseo Joaquín Romero <78494234+EliseoRom@users.noreply.github.com>
2021-09-01 16:13:25 -0300

    Componentes Home NavBar estructura basica

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+// import { NavLink } from 'react-router-dom';
+// import {Link } from 'react-router-dom';
+// import style from "./Home.module.css";
+
+function Home() {
+    
+    return (
+        <div className= 'body3'>
+           <NavBar/>
+            <h1> Hello Home InProgress</h1>
+           
+        </div>
+    )
+}
+
+export default Home
---
+++ client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./NavBar.module.css";
+// import Search from "../Search/Search";
+//---------NAVBAR--------//
+
+export default function NavBar() {
+  return (
+    <div className={s.navBar}>
+      <ul className={s.ulNav}>
+        <li>
+          <NavLink className={s.text} exact to="/CATEGORIAS">
+            CATEGORIAS
+          </NavLink>
+          <NavLink className={s.text} exact to="/OFERTAS">
+            OFERTAS
+          </NavLink>
+          <NavLink className={s.text} exact to="/LOGIN">
+            LOGIN
+          </NavLink>
+        </li>
+        <li>
+          {/* <Search /> */}
+        </li>
+      </ul>
+    </div>
+  );
+}
