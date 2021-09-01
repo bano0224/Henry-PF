de400067773febd17653f28a1e5697b15882b80f
Matías Chappet <matiaschappet@gmail.com>
2021-09-01 01:46:12 -0300

    Creacion de data y ruta get products
    Co-authored-by: mnaranjo1 <mnaranjo1@users.noreply.github.com>
    Co-authored-by: Bano <bano0224@users.noreply.github.com>

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,1 +1,10 @@
 const server = require('express').Router()
+const products = require('../data/products')
+
+
+server.get('/', async (req, res, next) => {
+    res.status(200).json(products)
+})
+
+
+module.exports = server;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const connectDB = require('./config/db');
 const express = require('express');
+const morgan = require("morgan");
+const routes = require('./routes/index')
 
 connectDB(); 
-
 const app = express();
+
+app.use("/", routes);
+app.use(express.json({ limit: '50mb' }))
+app.use(morgan("dev"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
