0c5301573c33f65696322e1f1c14bfafadb0d6cc
Eliseo Joaquín Romero <78494234+EliseoRom@users.noreply.github.com>
2021-09-01 16:07:35 -0300

    Componentes

---
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "./Landing.module.css";
+import { Link } from "react-router-dom";
+
+// Page Welcome go to home
+export default function LandingPage() {
+  return (
+    <div className={style.s}>
+      <h2 className={style.text}>Welcome the Supermarket Henry</h2>
+      <div className={style.setdirection}>
+        <Link to='/home'>
+          
+          <button className={style.button}>GO SHOPPING</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
