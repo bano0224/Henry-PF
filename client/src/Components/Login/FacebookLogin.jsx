f6b7bf44fb1d5703b927f7482f433d53b66b9d5f
Eliseo Joaquín Romero <78494234+EliseoRom@users.noreply.github.com>
2021-09-07 22:57:28 -0300

    login componente

---
+++ client/src/Components/Login/FacebookLogin.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FacebookLogin = ({ width = 24, height = 24, fill = "#000" }) => {
+  return (
+    <svg>
+      <path>
+
+      </path>
+    </svg>
+  );
+};
+
+export default FacebookLogin;
+
+
