58d9648654c381708c3ea767896dca3e21c3dea0
Bano <banorodriguez0224@gmail.com>
2021-09-09 01:23:39 -0300

    "Creación de rutas de loguin del back"
    Co-authored-by: Eliseo  <EliseoRom@users.noreply.github.com>
    Co-authored-by: mnaranjo1 <mnaranjo1@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
-import Home from "./Components/Home/Home";
-import CreateProduct from "./Components/CreateProduct/CreateProduct";
-import DetailProduct from "./Components/DetailProduct/DetailProduct"
-import AdminNav from "./Components/AdminDashboard/AdminNav/AdminNav";
-import AdminProduct from "./Components/AdminDashboard/AdminProduct/AdminProduct";
-import AdminUsers from "./Components/AdminDashboard/AdminUsers/AdminUsers";
-import AdminCategories from "./Components/AdminDashboard/AdminCategories/AdminCategories";
-import AdminAddProduct from "./Components/AdminDashboard/AdminAddProduct/AdminAddProduct";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Cart from "./Components/ShoppingCart/ShoppingCart";
-import AdminModifyProduct from "./Components/AdminDashboard/AdminModifyProduct/AdminModifyProduct";
-import AdminAddCategory from "./Components/AdminDashboard/AdminAddCategory/AdminAddCategory";
-import Reviews from "./Components/Reviews/Reviews";
-import Login from "./Components/Login/Login";
-=======
 import Home from "./components/Home/Home";
 import CreateProduct from "./components/CreateProduct/CreateProduct";
 import DetailProduct from "./components/DetailProduct/DetailProduct"
@@ -24,11 +8,12 @@ import AdminProduct from "./components/AdminDashboard/AdminProduct/AdminProduct"
 import AdminUsers from "./components/AdminDashboard/AdminUsers/AdminUsers";
 import AdminCategories from "./components/AdminDashboard/AdminCategories/AdminCategories";
 import AdminAddProduct from "./components/AdminDashboard/AdminAddProduct/AdminAddProduct";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Cart from "./components/ShoppingCart/ShoppingCart";
 import AdminModifyProduct from "./components/AdminDashboard/AdminModifyProduct/AdminModifyProduct";
 import AdminAddCategory from "./components/AdminDashboard/AdminAddCategory/AdminAddCategory";
 import Reviews from "./components/Reviews/Reviews";
->>>>>>> 8c2870145ad18c417539b72d38c4952817146840
+import Login from "./components/Login/Login";
 
 function App() {
   return (
