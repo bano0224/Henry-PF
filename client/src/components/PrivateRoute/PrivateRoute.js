4afd2032e7ba5ba181b8b6b6369fd406f3bc8552
mnaranjo1 <mgnaranjo@gmail.com>
2021-09-16 02:22:14 -0300

    Borré código comentado

--- client/src/components/PrivateRoute/PrivateRoute.js
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -1,16 +1,9 @@
 import {Redirect,Route} from 'react-router-dom';
 import jwt from 'jsonwebtoken'
 
-
-/* const PrivateRoute = (props) => {
-    return  ( <Route exact={props.exact} path={props.path} component={props.component}/>
-    )
-} */
 const numero = JSON.parse(sessionStorage.getItem("token"))?.token
 const decoded = numero? jwt.verify(numero, 'secret'):null
 const PrivateRoute = ({component:Component, ...rest}) => {
-    console.log(decoded?.role[0]?.name)
-
     return(
 
     <Route {...rest} render={props => (
