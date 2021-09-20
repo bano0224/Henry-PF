5573e977d23b18a5df0f00c605e166651b43c271
Matías Chappet <matiaschappet@gmail.com>
2021-09-20 01:10:20 -0300

    cartscreen actualizado

--- client/src/Components/Cart/CartScreen.jsx
+++ client/src/Components/Cart/CartScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import Cart from './Cart';
 import { makeStyles } from '@material-ui/core/styles';
@@ -175,51 +175,3 @@ export default function CartScreen() {
     )
 }
 
-
-// export default function SimpleAccordion() {
-//   const classes = useStyles();
-
-//   return (
-//     <div className={classes.root}>
-//       <Accordion>
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel1a-content"
-//           id="panel1a-header"
-//         >
-//           <Typography className={classes.heading}>Accordion 1</Typography>
-//         </AccordionSummary>
-//         <AccordionDetails>
-//           <Typography>
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-//             sit amet blandit leo lobortis eget.
-//           </Typography>
-//         </AccordionDetails>
-//       </Accordion>
-//       <Accordion>
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel2a-content"
-//           id="panel2a-header"
-//         >
-//           <Typography className={classes.heading}>Accordion 2</Typography>
-//         </AccordionSummary>
-//         <AccordionDetails>
-//           <Typography>
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-//             sit amet blandit leo lobortis eget.
-//           </Typography>
-//         </AccordionDetails>
-//       </Accordion>
-//       <Accordion disabled>
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel3a-content"
-//           id="panel3a-header"
-//         >
-//           <Typography className={classes.heading}>Disabled Accordion</Typography>
-//         </AccordionSummary>
-//       </Accordion>
-//     </div>
-//   );
-// }
